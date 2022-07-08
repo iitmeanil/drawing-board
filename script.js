9538299f66392ae533d9ad376ddc8b1563cbdf76
Anil Kumar Raju <iitmeanil@gmail.com>
2022-07-08 12:55:51 +0530

    adds header, reset and set size buttons
    the size of the squares need some reworking as it is not working
    properly

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-function startCreating () {
+let globalGridSize =16;
+function setSize() {
+    let num = prompt("Enter the Size of Grid you want (number between 10 and 100)",16);
+    if (+num >=10 && +num <=100){
+        startCreating (+num);
+    }
+}
+
+function startCreating (gridSize = 16) {
+    if (isNaN(gridSize)) {
+        gridSize = globalGridSize;
+    }
+    globalGridSize = gridSize;
     const body = document.querySelector('body');
-    let gridSize = 25;
+// clearing the body
+    let child = body.firstChild;
+    while(child) {
+        body.removeChild(child);
+        child = body.firstChild;
+    }
+
+
+// adding header and buttons
+    const heading = document.createElement("h1");
+    heading.classList.add("heading");
+    heading.textContent = "Sketch Pad";
+    body.appendChild(heading);
+
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("buttons");
+    body.appendChild(buttonsDiv);
+
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset";
+    resetButton.addEventListener('click',startCreating);
+    buttonsDiv.appendChild(resetButton);
+
+    const sizeButton = document.createElement("button");
+    sizeButton.textContent = "Set Size";
+    sizeButton.addEventListener ('click',setSize);
+    buttonsDiv.appendChild(sizeButton);
+
+    // creating grids of divs
     let newDiv = [];
-    let margin = 2;
+    let margin = 1;
     let box;
-    let boxHeight = Math.floor((window.innerHeight - (gridSize + 1)*margin - 150 )/gridSize);
-    console.log(boxHeight);
-    console.log(window.innerWidth, window.innerHeight);
+    let boxHeight = Math.floor((window.innerHeight - (gridSize + 1)*margin - 200 )/gridSize);
+    console.log(boxHeight, gridSize);
+    //console.log(window.innerWidth, window.innerHeight);
 
     for (let i = 0; i < gridSize; i++) {
         newDiv[i] = document.createElement("div");
